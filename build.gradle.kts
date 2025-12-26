6d1f98d619a23ec2008d669064d050ddeecc4cad
Alexander Krysin <alessandro56@yandex.ru>
2025-12-26 17:07:03 +0500

    Migrate Gradle build scripts from Groovy to Kotlin.

---
+++ build.gradle.kts
@@ -0,0 +1,40 @@
+plugins {
+    id("org.jetbrains.kotlin.jvm") version "2.3.0"
+    id("org.jetbrains.dokka") version "2.1.0"
+    id("com.vanniktech.maven.publish") version "0.35.0"
+}
+
+group = "io.github.obimp"
+version = "0.1.7"
+
+repositories {
+    mavenCentral()
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+dependencies {
+    implementation("io.github.obimp:obimp4j-core:0.1.9")
+    implementation("org.bouncycastle:bctls-jdk18on:1.83")
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "githubPackages"
+            url = uri("https://maven.pkg.github.com/obimp/obimp4j-client")
+            credentials(PasswordCredentials::class)
+        }
+    }
+}
+
+mavenPublishing {
+    coordinates(group as String, name as String, version as String)
+}
